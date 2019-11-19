c69637975abadd81548e0e2eabaf0af916e97e00
jack.luo <luohao518@yeah.net>
2019-11-19 10:28:42 +0800

    add zuul module

---
+++ importex-zuul/src/main/java/com/importexpress/Zuul.java
@@ -0,0 +1,21 @@
+package com.importexpress;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+/**
+ * @author luohao
+ */
+
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul.class, args);
+    }
+}
