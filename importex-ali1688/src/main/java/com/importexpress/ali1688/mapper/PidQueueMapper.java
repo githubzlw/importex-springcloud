36f71d6f5ca267101c48691f7c8e33a13cc47997
jack.luo <luohao518@yeah.net>
2019-12-02 15:49:44 +0800

    fixed annotation

--- importex-ali1688/src/main/java/com/importexpress/ali1688/mapper/PidQueueMapper.java
+++ importex-ali1688/src/main/java/com/importexpress/ali1688/mapper/PidQueueMapper.java
@@ -4,7 +4,7 @@ import com.importexpress.ali1688.model.PidQueue;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
-@Component
-@org.apache.ibatis.annotations.Mapper
+//@Component
+//@org.apache.ibatis.annotations.Mapper
 public interface PidQueueMapper extends Mapper<PidQueue> {
 }
