f2777eabf35dfd04fcb0a3cf700cc8f8a4550d00
jack.luo <luohao518@yeah.net>
2019-12-12 14:08:08 +0800

    feat(compile): add product module

---
+++ importex-product/src/main/java/com/importexpress/product/aop/LogAspect.java
@@ -0,0 +1,38 @@
+package com.importexpress.product.aop;
+
+import com.importexpress.comm.util.AopLogUtil;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author luohao
+ * @date 2019/11/27
+ */
+@Aspect
+@Service
+public class LogAspect {
+
+    @Pointcut("execution(* com.importexpress.product.rest..*.*(..))")
+    public void controlLog() {
+    }
+
+    @Around("controlLog()")
+    public Object controlAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        return AopLogUtil.watchMethod(joinPoint);
+    }
+
+    @Pointcut("execution(* com.importexpress.product.service..*.*(..))")
+    public void serviceLog() {
+    }
+
+
+    @Around("serviceLog()")
+    public Object serviceAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        return AopLogUtil.watchMethod(joinPoint);
+    }
+}
