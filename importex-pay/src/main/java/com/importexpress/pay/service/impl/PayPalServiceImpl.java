4cb15ef4fc5ffe89a7374dee9f36333daf4246c9
jack.luo <luohao518@yeah.net>
2019-12-16 14:29:51 +0800

    [bug]:json -> no json

--- importex-pay/src/main/java/com/importexpress/pay/service/impl/PayPalServiceImpl.java
+++ importex-pay/src/main/java/com/importexpress/pay/service/impl/PayPalServiceImpl.java
@@ -183,7 +183,7 @@ public class PayPalServiceImpl implements PaypalService {
             }catch(IllegalArgumentException iae){
                 log.error("refund",iae);
             }
-            return CommonResult.success(detailedRefund.toJSON());
+            return CommonResult.success(detailedRefund);
         } catch (PayPalRESTException e) {
             return CommonResult.failed(e.getMessage());
         }
