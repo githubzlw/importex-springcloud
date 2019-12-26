80d8c00685e7655ec95d5f8fde485312201e0595
shenjing <shenjing@PC-201902261143>
2019-12-26 10:46:17 +0800

    其他页面相关的搜索逻辑

--- importex-search/src/main/java/com/importexpress/search/common/ProductSearch.java
+++ importex-search/src/main/java/com/importexpress/search/common/ProductSearch.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
