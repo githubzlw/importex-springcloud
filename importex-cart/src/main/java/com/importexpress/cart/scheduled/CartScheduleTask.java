bb4b5a9b6f74ba8eb239c4472588832bad9cd896
jack.luo <luohao518@yeah.net>
2020-06-22 16:33:17 +0800

    減小數組size：500 -> 50

--- importex-cart/src/main/java/com/importexpress/cart/scheduled/CartScheduleTask.java
+++ importex-cart/src/main/java/com/importexpress/cart/scheduled/CartScheduleTask.java
@@ -89,7 +89,7 @@ public class CartScheduleTask {
 
 
         StringBuilder sb = new StringBuilder();
-        List<List<Cart>> parts = Lists.partition(carts, 500);
+        List<List<Cart>> parts = Lists.partition(carts, 50);
         parts.stream().forEach(list -> {
 
             ByteArrayOutputStream out;
