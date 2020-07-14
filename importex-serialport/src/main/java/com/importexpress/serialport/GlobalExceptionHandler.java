f0b1ef08262f79f6f3b96d210d0952ae4cd544c7
jack.luo <luohao518@yeah.net>
2020-07-14 16:04:19 +0800

    点亮报警灯

--- importex-serialport/src/main/java/com/importexpress/serialport/GlobalExceptionHandler.java
+++ importex-serialport/src/main/java/com/importexpress/serialport/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.importexpress.serialport;
 
 import com.importexpress.serialport.exception.SerialPortException;
 import com.importexpress.serialport.mq.SendMQ;
+import com.importexpress.serialport.service.SerialPort2Service;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,6 +14,9 @@ class GlobalExceptionHandler {
 
     private final SendMQ sendMQ;
 
+    @Autowired
+    SerialPort2Service serialPort2Service;
+
     public GlobalExceptionHandler(SendMQ mq) {
         this.sendMQ = mq;
     }
@@ -20,6 +25,8 @@ class GlobalExceptionHandler {
     public void defaultErrorHandler(SerialPortException e) throws Exception {
 
         log.error("SerialPortException handler:", e);
+        //点亮报警灯
+        serialPort2Service.warningLight(true);
         sendMQ.sendWarningMsgToMQ(e.toString());
 
     }
--- importex-serialport/src/main/java/com/importexpress/serialport/exception/SerialPortException.java
+++ importex-serialport/src/main/java/com/importexpress/serialport/exception/SerialPortException.java
@@ -33,8 +33,7 @@ public class SerialPortException extends RuntimeException{
     private final String msg;
     /** 转盘操作失败*/
     public final static int SERIAL_PORT_EXCEPTION_TURN_TABLE_ERROR = 1006;
-    @Autowired
-    SerialPort2Service serialPort2Service;
+
     public SerialPortException(int code){
         this.code = code;
         switch (code){
@@ -62,8 +61,7 @@ public class SerialPortException extends RuntimeException{
             default:
                 this.msg = "";
         }
-        //点亮报警灯
-        serialPort2Service.warningLight(true);
+
     }
 
     @Override
